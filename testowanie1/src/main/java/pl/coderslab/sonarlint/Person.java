5da5b65bb47316dbefe4805ec6e1d898553decd2
“nkopczyn” <nkopczyn@visa.com>
2025-08-05 13:13:35 +0200

    sonarlint

---
+++ testowanie1/src/main/java/pl/coderslab/sonarlint/Person.java
@@ -0,0 +1,30 @@
+package pl.coderslab.sonarlint;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+
+
+    public String getFirstName() {
+        System.out.println("Getting FirstName");
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getInitials() {
+        System.out.println(this.firstName.charAt(0) + "." + this.lastName.charAt(0) + ".");
+    }
+
+}
+
